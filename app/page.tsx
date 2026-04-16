a7f763b6558f49b05810e434184e60f41ee3ff7f
Anuradha Sharma <anuradhasharma71440@gmail.com>
2026-04-16 12:46:47 +0530

    feat : basic search input ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@
 
 export default function Home() {
   return (
-<div></div>
+   <main className="flex items-center justify-center h-screen bg-black text-white">
+    <input type="text" 
+    placeholder="Type !leet dp"
+    className="w-[400px] p-4 rounded-xl bg-zinc-900 border border-zinc-700 focus:outline-none focus:ring-2 focus:ring-white"
+    />
+   </main>
   );
 }
