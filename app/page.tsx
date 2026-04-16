15d1498b92ad27d29a05346771c62b3b71bbff73
Anuradha Sharma <anuradhasharma71440@gmail.com>
2026-04-16 12:59:04 +0530

    feat: command parsing and redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,44 @@
-
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+
+  const handleSearch = () => {
+    if (!input.startsWith("!")) return;
+
+    const [cmd, ...rest] = input.slice(1).split(" ");
+    const query = rest.join(" ");
+
+    let url = " ";
+
+    if (cmd === "leet") {
+      url = `https://leetcode.com/problemset/?search=${query}`;
+    } else if (cmd === "yt") {
+      url = `https://www.youtube.com/results?search_query=${query}`;
+    } else if (cmd === "gate") {
+      url = `https://www.google.com/search?q=gate+${query}`;
+    }
+
+    if (url) {
+      window.location.href = url;
+    } else {
+      alert("Command not found");
+    }
+  };
+
   return (
-   <main className="flex items-center justify-center h-screen bg-black text-white">
-    <input type="text" 
-    placeholder="Type !leet dp"
-    className="w-[400px] p-4 rounded-xl bg-zinc-900 border border-zinc-700 focus:outline-none focus:ring-2 focus:ring-white"
-    />
-   </main>
+    <main className="flex items-center justify-center h-screen bg-black text-white">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleSearch();
+        }}
+        placeholder="Type !leet dp"
+        className="w-100 p-4 rounded-xl bg-zinc-900 border border-zinc-700 focus:outline-none focus:ring-2 focus:ring-white"
+      />
+    </main>
   );
 }
