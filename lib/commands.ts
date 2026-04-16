825c7cc081b13f5c96b4e448ec7aad65293c44d2
Anuradha Sharma <anuradhasharma71440@gmail.com>
2026-04-16 13:19:21 +0530

    feat: add useful command mappings

---
+++ lib/commands.ts
@@ -0,0 +1,11 @@
+export const commands: Record<string, string> = {
+    leet: "https://leetcode.com/problemset/?search=",
+    yt: "https://www.youtube.com/results?search_query=",
+    google: "https://www.google.com/search?q=",
+    github: "https://github.com/search?q=",
+    npm: "https://www.npmjs.com/search?q=",
+    docs: "https://developer.mozilla.org/en-US/search?q=",
+    gate: "https://www.google.com/search?q=gate+",
+    intern: "https://www.google.com/search?q=internships+",
+    roadmap: "https://roadmap.sh/",
+}
